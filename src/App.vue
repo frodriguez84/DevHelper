36b41ef2a5fe6daab216fe375a8344ea22c00494
frodriguez84 <fernando.rodriguez84@yahoo.com.ar>
2021-10-30 16:30:58 -0300

    asd

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,30 @@
         <b-navbar-toogle target="nav_collapse"></b-navbar-toogle>
           
         <b-navbar-brand href="#">DevHelper</b-navbar-brand>
-         <!--<b-navbar-brand class="user">Usuario </b-navbar-brand>-->
+         <!--<b-navbar-brand class="user">Bienvenido: </b-navbar-brand>-->
         <b-collapse is-nav id="nav_collapse"> 
           <b-navbar-nav>
             
             <b-nav-item :to="{name: 'Home'}" active-class="active" exact>Home</b-nav-item>
+            <template v-if="$store.state.auth">
+              <span >
+              <b-navbar-brand class="user">Bienvenido: </b-navbar-brand>
+              <b-nav-item :to="{name: 'Login'}" active-class="active" exact>Logout</b-nav-item>
+              </span>
+            </template>
+            <template v-else>
             <b-nav-item :to="{name: 'Login'}" active-class="active" exact>Login</b-nav-item>
+
+            </template>
             <b-nav-item :to="{name: 'Registro'}" active-class="active" exact>Registro</b-nav-item>
             <b-nav-item :to="{name: 'Principal'}" active-class="active" exact>Principal</b-nav-item>
             <b-nav-item :to="{name: 'Tienda'}" active-class="active" exact>Tienda</b-nav-item>
             <b-nav-item :to="{name: 'Producto'}" active-class="active" exact>Producto</b-nav-item>
             <b-nav-item :to="{name: 'ModuloUsuario'}" active-class="active" exact>Modulo Usuario</b-nav-item>
           </b-navbar-nav>
-
+            <b-navbar-nav>
+              <!-- <b-nav-item class="user">Bienvenido: {{usuarios.nombre}}</b-nav-item> -->
+            </b-navbar-nav>
         </b-collapse>
       </b-container>
     </b-navbar>
@@ -30,19 +41,30 @@
             </div>
         </div>
     </footer>
-    
+    <div>
+          <AgregarUsuario/>
+        </div>
   </div>
 </template>
 
 <script>
 
 import HelloWorld from './components/HelloWorld.vue'
+import AgregarUsuarios from './components/AgregarUsuario.vue'
+import { mapGetters } from "vuex";
 
 export default {
   name: 'app',
+
   components:{
-    HelloWorld
-  }
+    HelloWorld,
+    AgregarUsuarios
+    
+  },
+  computed: {
+    ...mapGetters({ usuarios: "getUsuarios" }),
+  },
+  
 }
 </script>
 
@@ -89,6 +111,12 @@ footer.sticky-footer .copyright {
     width: calc(100% - 0px);
   }
   
+  .user{
+    position:absolute;
+    width:20%;
+    right: 5%;
+    text-align:center;
+  }
 }
 </style>  
 
